10a800d605a9869f6cedd9b0e200ecd9549d9c47
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-07-18 13:51:35 -0300

    feat: add media service

---
+++ service/media.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"github.com/RianNegreiros/short-video-system/models"
+	"github.com/RianNegreiros/short-video-system/utils"
+	"github.com/go-playground/validator/v10"
+)
+
+var (
+	validate = validator.New()
+)
+
+type mediaUpload interface {
+	FileUpload(file models.File) (string, error)
+	RemoteUpload(url models.Url) (string, error)
+}
+
+type media struct{}
+
+func NewMediaUpload() mediaUpload {
+	return &media{}
+}
+
+func (*media) FileUpload(file models.File) (string, error) {
+	err := validate.Struct(file)
+	if err != nil {
+		return "", err
+	}
+
+	uploadUrl, err := utils.ImageUploadHelper(file.File)
+	if err != nil {
+		return "", err
+	}
+	return uploadUrl, nil
+}
+
+func (*media) RemoteUpload(url models.Url) (string, error) {
+	err := validate.Struct(url)
+	if err != nil {
+		return "", err
+	}
+
+	uploadUrl, errUrl := utils.ImageUploadHelper(url.Url)
+	if errUrl != nil {
+		return "", err
+	}
+	return uploadUrl, nil
+}
