b56baa762ca1294f44851d3ac673dca17410d999
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-07-18 13:43:40 -0300

    refactor: load godotenv on main

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"log"
+
 	"github.com/RianNegreiros/short-video-system/controllers"
 	"github.com/RianNegreiros/short-video-system/middlewares"
 	"github.com/RianNegreiros/short-video-system/models"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
 	models.ConnectDataBase()
 
 	r := gin.Default()
