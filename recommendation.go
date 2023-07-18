5d54104ea223e485400d75216e4b63c8672255da
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-07-18 12:02:00 -0300

    feat: create get recommendations using jaccard similarity

--- recommendation.go
+++ recommendation.go
@@ -8,3 +8,40 @@ type Video struct {
 	Category    string
 	Tags        []string
 }
+
+func GetRecommendations(userID string, videos []Video, userVideos []Video) []Video {
+	recommendations := []Video{}
+	threshold := 0.5
+
+	for _, userVideo := range userVideos {
+		for _, video := range videos {
+			if video.UserID != userID {
+				categorySimilarity := jaccard([]string{userVideo.Category}, []string{video.Category})
+				tagsSimilarity := jaccard(userVideo.Tags, video.Tags)
+				averageSimilarity := (categorySimilarity + tagsSimilarity) / 2
+
+				if averageSimilarity >= threshold {
+					recommendations = append(recommendations, video)
+				}
+			}
+		}
+	}
+
+	return recommendations
+}
+
+func jaccard(setA, setB []string) float64 {
+	intersection := 0
+	union := len(setA) + len(setB)
+	for _, a := range setA {
+		for i, b := range setB {
+			if a == b {
+				intersection++
+				setB = append(setB[:i], setB[i+1:]...)
+				break
+			}
+		}
+	}
+	union -= intersection
+	return float64(intersection) / float64(union)
+}
