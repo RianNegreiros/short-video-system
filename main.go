0a778693fc572ca06ab7ab6f7bb2bd7d9c267e99
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-08-27 12:22:36 -0300

    refactor: cleanup

--- main.go
+++ main.go
@@ -1,39 +1,11 @@
 package main
 
 import (
-	"log"
-
-	"github.com/RianNegreiros/short-video-system/controllers"
-	"github.com/RianNegreiros/short-video-system/middlewares"
-	"github.com/RianNegreiros/short-video-system/models"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load(".env")
+	router := gin.Default()
 
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-
-	models.ConnectDataBase()
-
-	r := gin.Default()
-
-	public := r.Group("/api")
-
-	public.POST("/register", controllers.Register)
-	public.POST("/login", controllers.Login)
-
-	upload := r.Group("/api/upload")
-	upload.Use(middlewares.JwtAuthMiddleware())
-	upload.POST("/file", controllers.FileUpload())
-	upload.POST("/url", controllers.RemoteUpload())
-
-	protected := r.Group("/api/admin")
-	protected.Use(middlewares.JwtAuthMiddleware())
-	protected.GET("/user", controllers.CurrentUser)
-
-	r.Run(":8080")
+	router.Run(":8080")
 }
